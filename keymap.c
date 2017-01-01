0bfd449f099a88354cf265474b33946d6bd06986
J Shamsul <jibone@gmail.com>
2017-01-02 03:53:30 +0800

    imlement improvements

--- keymap.c
+++ keymap.c
@@ -51,28 +51,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
   [_QWERTY] = {
-    { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC },
-    { KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT  },
-    { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT },
-    { KC_LCTL, KC_ESC,  KC_LALT, KC_LGUI, LOWER,   LT(SPACE, KC_SPC),  LT(SPACE, KC_SPC),  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT }
+    { LT(KC_LSFT, KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,               KC_U,    KC_I,    KC_O,    KC_P,    LT(KC_RSFT, KC_BSPC) },
+    { KC_LCTL,              KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,               KC_J,    KC_K,    KC_L,    KC_SCLN, SFT_ENT              },
+    { KC_LSFT,              KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,               KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT              },
+    { KC_LCTL,              KC_ESC,  KC_LALT, KC_LGUI, LOWER,   LT(SPACE, KC_SPC),  LT(SPACE, KC_SPC),  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT              }
   },
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
+ * | Del  |      |      |      |      |      |      |   _  |   +  |   {  |   }  |  |   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |      |   "  |      |
+ * |      |      |      |      |      |      |      |      |      |      |   "  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
   [_LOWER] = {
-    { KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC },
-    { KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE },
-    { _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, KC_DQUO, _______ },
-    { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ }
+    { KC_TILD, KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,    KC_CIRC,    KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC },
+    { KC_DEL,  LGUI(KC_A), LGUI(KC_S), LGUI(KC_D), _______,    _______,    _______,    KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE },
+    { _______, LGUI(KC_Z), LGUI(KC_X), LGUI(KC_C), LGUI(KC_V), LGUI(KC_B), LGUI(KC_N), _______, _______, _______, KC_DQUO, _______ },
+    { _______, _______,    _______,    _______,    _______,    _______,    _______,    _______, _______, _______, _______, _______ }
   },
 
 /* Raise
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
   [_SPACE] = {
-    { _______, _______, KC_MS_U, _______, _______, KC_WH_D, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, KC_BTN5, _______ },
+    { KC_ESC,  _______, KC_MS_U, _______, _______, KC_WH_D, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, KC_BTN5, _______ },
     { _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, KC_WH_U, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______ },
     { _______, _______, _______, _______, _______, KC_WH_L, KC_WH_R, _______, _______, _______, _______, _______ },
     { KC_SLEP, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY }
